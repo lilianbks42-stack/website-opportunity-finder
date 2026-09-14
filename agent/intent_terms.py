3eb4f5bc1b7f40861e86d0b940fa049490259734
Bee <lilianbks42@gmail.com>
2026-09-14 21:02:59 +0300

    Create intent_terms.py

---
+++ agent/intent_terms.py
@@ -0,0 +1,12 @@
+INTENT_TERMS = [
+    "looking for a website developer",
+    "need a landing page",
+    "need someone to build a website",
+    "looking for a web designer",
+    "need a website for my business",
+    "looking for someone to create a website",
+    "website developer needed",
+    "how do I get my business online",
+    "looking for someone to build my website",
+    "need an online presence",
+]
